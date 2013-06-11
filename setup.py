5f909146fcf783353f452bc60d6bff8b09091d4f
Jordan Carlson <jwgcarlson@gmail.com>
2013-06-10 19:39:41 -0700

    Package up pcap2har nicely using setuptools.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+import pcap2har
+
+setup(
+    name="pcap2har",
+    version=pcap2har.__version__,
+    description="PCAP to HAR conversion utility",
+    long_description="A simple tool to generate HTTP Archive (HAR) files from PCAP-formatted network capture data.",
+    author="Andrew Fleenor",
+    author_email="no@reply.com",
+    maintainer="Jordan Carlson",
+    maintainer_email="jwgcarlson@gmail.com",
+    url="https://github.com/jwgcarlson/pcap2har",
+    packages=["pcap2har"],
+    scripts=["bin/pcap2har"],
+)
